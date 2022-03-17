0b3ae8867941b06924534c818b1da48043e8dea4
mohan <mohanreddy0532@gmail.com>
2022-03-17 19:06:37 +0530

    evenodd

--- evenodd.ps1
+++ evenodd.ps1
@@ -1,4 +1,11 @@
 $numbers = 1..10
 foreach($number in $numbers) {
-write-host =
+write-host "The number is cal" $number
+if(($number%2) -eq 0) {
+    Write-Host "NUmber $number is even"
+    }
+else
+{
+    Write-Host "Number is Odd"
+}
 }
