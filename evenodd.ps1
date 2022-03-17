0661f6df4dde937f9c4fd658c63fac1e521f05b7
mohan <mohanreddy0532@gmail.com>
2022-03-17 18:56:19 +0530

    test

---
+++ evenodd.ps1
@@ -0,0 +1,4 @@
+$numbers = 1..10
+foreach($number in $numbers) {
+write-host =
+}
---
+++ powershell.ps1
@@ -0,0 +1,17 @@
+#$PSVersionTable
+Write-Host "Hello World!!" -ForegroundColor Green
+#Ex:
+#$namelist = @('Priya','Mohan','Cathy')
+#$location = @('Hyd','Ban','India')
+#for ($i=0;$i -lt 3;$i++){
+#>> Write-Host "My Name is" $namelist[$i]  "I m from" $location[$i]
+#>> }
+#My Name is Priya I m from Hyd
+#My Name is Mohan I m from Ban
+#My Name is Cathy I m from India
+#PS /Users/mohan/Desktop/devops>
+#----
+#PS /Users/mohan/Desktop/devops> Get-ExecutionPolicy
+#Unrestricted
+#PS /Users/mohan/Desktop/devops> Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+#PS /Users/mohan/Desktop/devops> Install-Module -Name AZ
