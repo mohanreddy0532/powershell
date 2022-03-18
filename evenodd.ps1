405e074fb87a307c2b8b1832230f8b9489001997
mohan <mohanreddy0532@gmail.com>
2022-03-18 06:56:59 +0530

    hello tf var desc.

--- evenodd.ps1
+++ evenodd.ps1
@@ -1,11 +1,17 @@
-$numbers = 1..10
+$numbers = 1..1000
+$evennumber=@()
+$oddnumber=@()
+
 foreach($number in $numbers) {
 write-host "The number is cal" $number
 if(($number%2) -eq 0) {
     Write-Host "NUmber $number is even"
+    $evennumber+=$number
     }
-else
-{
-    Write-Host "Number is Odd"
+else{
+    Write-Host "Number $number is Odd"
+    $oddnumber+=$number
 }
 }
+
+$evennumber|out.file "/Users/mohan/Desktop/devops/powershell/even.txt"
